fc561ae2a87ce5cc0fdbda4538e8051f61c377a4
Lezhi Li <lezhi.li@uber.com>
2019-07-29 21:30:21 -0700

    fix build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,24 @@
 /* eslint-env node */
 module.exports = {
-  extends: [require.resolve('eslint-config-fusion')],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:jest/recommended",
+    "eslint-config-uber-universal-stage-3"
+  ],
   env: {
     jest: true,
     node: true,
   },
+  settings: {
+    "react": {
+      "version" : "16.6.1"
+    }
+  },
+  plugins: [
+    "eslint-plugin-react"
+  ],
   rules: {
+    "react/prop-types": 'off',
     'import/no-unresolved': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/no-dynamic-require': 'off',
