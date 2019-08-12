11377d6d994467001d278e12ea318ca3d98faa02
Hong Wang <kennsmacintosh@gmail.com>
2019-08-12 09:45:15 -0700

    Jupyter utils (#1)
    * jupyter-utils
    
    * jupyter-utils
    
    * global
    
    * move the code base to bindings
    
    * clean up setup.py
    
    * licence
    
    * eslint fix
    
    * refine jupyter-externals
    
    * fix
    
    * remove dotenv

---
+++ binding/jupyter-utils/js/webpack.config.js
@@ -0,0 +1,39 @@
+const {resolve} = require('path');
+const webpack = require('webpack');
+const jupyterExternals = require('./jupyter-externals');
+
+const externals = [...jupyterExternals];
+
+module.exports = {
+  entry: {
+    app: [resolve('./src/index.js')],
+  },
+  devtool: 'source-maps',
+  output: {
+    path: resolve('dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: [/node_modules/],
+        include: [resolve('.')],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+    ],
+  },
+  externals,
+};
