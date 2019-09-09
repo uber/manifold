797a3b16af4f1571da2519e11bd859630c2b9546
Hong Wang <hongw@uber.com>
2019-09-09 11:51:21 -0700

    use ocular for mono repo structure

--- alias.config.js
+++ alias.config.js
@@ -2,18 +2,18 @@ const resolve = require('path').resolve;
 const fs = require('fs');
 
 const ROOT = resolve(__dirname, '.');
-const PACKAGES_ROOT = resolve(ROOT, 'src');
+const MODULES_ROOT = resolve(ROOT, 'modules');
 const ROOT_ALIAS = 'packages';
 
 // this function looks into all packages under ./src and creates alias for local dev
 // {@uber/some-package: './src/some-package'}
 module.exports = () =>
-  fs.readdirSync(PACKAGES_ROOT).reduce((aliasMap, pkg) => {
+  fs.readdirSync(MODULES_ROOT).reduce((aliasMap, pkg) => {
     // src/some-package
-    const packagePath = resolve(PACKAGES_ROOT, pkg);
+    const packagePath = resolve(MODULES_ROOT, pkg);
     if (!fs.lstatSync(packagePath).isDirectory()) {
       return aliasMap;
     }
-    aliasMap[`${ROOT_ALIAS}/${pkg}`] = resolve(PACKAGES_ROOT, pkg);
+    aliasMap[`${ROOT_ALIAS}/${pkg}`] = resolve(MODULES_ROOT, pkg, 'src');
     return aliasMap;
   }, {});
---
+++ ocular-dev-tools.config.js
@@ -0,0 +1,10 @@
+const {resolve} = require('path');
+
+const config = {
+  lint: {
+    paths: ['modules', 'examples'],
+    extensions: ['js'],
+  },
+};
+
+module.exports = config;
