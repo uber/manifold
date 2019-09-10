fb51f70cacd3649a429275aa8dc2a1fd0bc1a109
Hong Wang <hongw@uber.com>
2019-09-09 17:14:28 -0700

    Monorepo Structure (Using Ocular) (#18)
    Using the monorepo structure for the repository (leveraging ocular).

--- alias.config.js
+++ alias.config.js
@@ -2,18 +2,35 @@ const resolve = require('path').resolve;
 const fs = require('fs');
 
 const ROOT = resolve(__dirname, '.');
-const PACKAGES_ROOT = resolve(ROOT, 'src');
+const MODULES_ROOT = resolve(ROOT, 'modules');
 const ROOT_ALIAS = 'packages';
 
 // this function looks into all packages under ./src and creates alias for local dev
 // {@uber/some-package: './src/some-package'}
 module.exports = () =>
-  fs.readdirSync(PACKAGES_ROOT).reduce((aliasMap, pkg) => {
+  fs.readdirSync(MODULES_ROOT).reduce((aliasMap, pkg) => {
     // src/some-package
-    const packagePath = resolve(PACKAGES_ROOT, pkg);
+    const packagePath = resolve(MODULES_ROOT, pkg);
     if (!fs.lstatSync(packagePath).isDirectory()) {
       return aliasMap;
     }
-    aliasMap[`${ROOT_ALIAS}/${pkg}`] = resolve(PACKAGES_ROOT, pkg);
+    // packages/some-package/package.json
+    const packageJsonPath = resolve(packagePath, 'package.json');
+    // bypass alias mapping if no package.json exists
+    if (!fs.existsSync(packageJsonPath)) {
+      return aliasMap;
+    }
+    const packageInfo = require(packageJsonPath);
+    // @mlvis/some-package => some-package
+    const packageName = packageInfo.name.replace(/^(@mlvis\/)/, '');
+
+    // TODO: use this instead when each module is done refactoring
+    // {@mlvis/some-package: './modules/some-package/src'}
+    // aliasMap[packageInfo.name] = resolve(MODULES_ROOT, packageName, 'src');
+
+    // TODO: replace all reference to packages/... to @mlvis/some-package inside
+    // each module
+    // {'packages/some-package' : './modules/some-package/src'}
+    aliasMap[`${ROOT_ALIAS}/${pkg}`] = resolve(MODULES_ROOT, pkg, 'src');
     return aliasMap;
   }, {});
