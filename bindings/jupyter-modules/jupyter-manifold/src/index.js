d0b5a9f41dd7e928881819cc82563f4e46d70ee4
Hong Wang <hongw@uber.com>
2019-09-23 09:23:59 -0700

    jupyter-manifold: use a seperate store for each instance of manifold (#45)

--- bindings/jupyter-modules/jupyter-manifold/src/index.js
+++ bindings/jupyter-modules/jupyter-manifold/src/index.js
@@ -10,16 +10,20 @@ import {window} from 'global';
 // https://github.com/jhen0409/react-native-debugger/issues/280
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  manifoldReducer,
-  composeEnhancer(applyMiddleware(...enhanceReduxMiddleware([thunk])))
-);
-
-export default props => (
-  <Provider store={store}>
-    <React.Fragment>
-      <Controls widgetModel={props.widgetModel} widgetView={props.widgetView} />
-      <Manifold {...props} />
-    </React.Fragment>
-  </Provider>
-);
+export default props => {
+  const store = createStore(
+    manifoldReducer,
+    composeEnhancer(applyMiddleware(...enhanceReduxMiddleware([thunk])))
+  );
+  return (
+    <Provider store={store}>
+      <React.Fragment>
+        <Controls
+          widgetModel={props.widgetModel}
+          widgetView={props.widgetView}
+        />
+        <Manifold {...props} />
+      </React.Fragment>
+    </Provider>
+  );
+};
