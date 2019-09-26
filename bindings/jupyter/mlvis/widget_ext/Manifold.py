8439d39f9009bba9dddab446516184d689d38b42
Hong Wang <hongw@uber.com>
2019-09-26 13:20:27 -0400

    addressing review comments

--- bindings/jupyter/mlvis/widget_ext/Manifold.py
+++ bindings/jupyter/mlvis/widget_ext/Manifold.py
@@ -21,6 +21,7 @@ class Manifold(CommonComponent):
             else:
                 raise exception
 
+        # make a shallow copy of the props so that the original props dict remain unchanged
         processed_props = props.copy()
         processed_props['data']['x'] = self.process_x(data['x'])
         processed_props['data']['yPred'] = self.process_y_pred(data['yPred'])
