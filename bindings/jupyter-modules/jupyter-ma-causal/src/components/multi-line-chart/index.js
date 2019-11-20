26a50dcdeb1512938d4544edb909c977e12dea9e
Hong Wang <hongw@uber.com>
2019-11-20 13:52:58 -0800

    wip

--- bindings/jupyter-modules/jupyter-ma-causal/src/components/multi-line-chart/index.js
+++ bindings/jupyter-modules/jupyter-ma-causal/src/components/multi-line-chart/index.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {line as d3Line} from 'd3-shape';
+import {scaleLinear} from 'd3-scale';
 
 export default class Chart extends Component {
   static propTypes = {
     data: PropTypes.object.isRequired,
   };
   static defaultProps = {};
-  render() {}
+
+  _renderAxies() {}
+  _renderGrids() {}
+  _renderLines() {
+    const {data} = this.props;
+    if (!data || !data.lines) {
+      return null;
+    }
+    const {lines} = data;
+  }
+  render() {
+    return <svg />;
+  }
 }
